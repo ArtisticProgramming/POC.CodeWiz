adfa3137eef658abaac8af980ae853f6b96c89ef
Mostafa <36862442+ArtisticProgramming@users.noreply.github.com>
2023-08-25 17:21:13 +0200

    Initial commit

---
+++ CodeWiz/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POC.CodeWiz
+{
+    public class App
+    {
+        private readonly IHelper _helper;
+        public App(IHelper helper)
+        {
+            _helper = helper;
+        }
+
+        public void Run()
+        {
+            //_helper.DoSomething();
+        }
+    }
+}
---
+++ CodeWiz/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using POC.CodeWiz;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using IHost host = CreateHostBuilder(args).Build();
+        using var scope = host.Services.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        var app = serviceProvider.GetRequiredService<App>();
+        app.Run();
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
+             .ConfigureServices((hostContext, services) =>
+             {
+                 services.AddTransient<IHelper, Helper>();
+                 services.AddScoped<App>();
+             });
+}
